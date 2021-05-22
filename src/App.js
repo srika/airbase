ba0a5a496d2df9ed011e838fd7cd462511f22656
srika <srikanthnayak583@gmail.com>
2021-05-22 01:03:42 -0400

    code assessment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //components
-import Review from "./components/Review";
+import RequestApproval from "./components/RequestApproval";
 //styles
 import "./styles/App.scss";
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="main-container">
-        <Review />
+        <RequestApproval />
       </div>
     </div>
   );
